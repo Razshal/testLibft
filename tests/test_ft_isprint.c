124b847d31444561ab4326555db63bb78706c43b
Mathieu FONTENILLE <mfonteni@e3r3p5.42.fr>
2017-11-14 20:23:38 +0100

    progress

--- tests/test_ft_isprint.c
+++ tests/test_ft_isprint.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 19:02:38 by mfonteni          #+#    #+#             */
-/*   Updated: 2017/11/14 19:08:10 by mfonteni         ###   ########.fr       */
+/*   Updated: 2017/11/14 19:54:59 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@ void	test_ft_isprint(void)
 {
 	int i = -5;
 
-	while (i++ < 500)
+	while (i < 500)
 	{
 		if (ft_isprint(i) != isprint(i))
 		{
 			PRINTFFAILURE;
 			printf("Echec avec la valeur %d, %c", i, i);
 		}
+		i++;
 	}
 }
